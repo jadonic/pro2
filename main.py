e6ad3c2ed6712529a012241b8e3c3401a1d32e23
jadonic <21343764+jadonic@users.noreply.github.com>
2021-12-20 15:37:38 +0100

    Diverse Anpassungen und neue Funktionen, erster Aufbau Datenspeicher und Gewichtereinteilung

--- main.py
+++ main.py
@@ -1,118 +1,89 @@
-import jinja2
-import random
-import datenspeicher
+import json
 from flask import Flask
 from flask import render_template
 from flask import request
+from random import randrange
 
 app = Flask("Hello World")
 
 
 @app.route("/hello", methods=["GET", "POST"])
 def hello_post():
-    # von https://stackoverflow.com/questions/19794695/flask-python-buttons
+    r = open("ringerdatenbank.json")
+    ringerdatenbank_dict = json.load(r)
+
+    t = open("turnierdatenbank.json")
+    turnierdatenbank_dict = json.load(t)
+
+    loszuteilung = [0]
+    aktiveslos = 0
+    ringerID = 100000
+
     if request.method == "POST":
         if request.form.get("eintragen") == "Eintragen":
-            # global variables von hier: https://www.w3schools.com/python/python_variables_global.asp
-            global vollername
-            vollername = request.form["vollername"]
+            ringerID = ringerID + 1
+            # nimmt daten von erstem Formular entgegen
+            vorname = request.form["vorname"]
+            nachname = request.form["nachname"]
+            land = request.form["land"]
             gewicht = request.form["gewicht"]
-            counter57 = 0
-            counter65 = 0
-            counter70 = 0
-            counter79 = 0
-            counter96 = 0
-            counter130 = 0
-            gesamtcounter = counter65 + counter70 + counter79 + counter96 + counter130
-            if int(gewicht) <= 57 and counter57 <= 8:
-                datenspeicher.gewichtsklassen[57][vollername] = {}
-                datenspeicher.gewichtsklassen[57][vollername]["Gewicht"] = gewicht
-                datenspeicher.gewichtsklassen[57][vollername]["Name"] = vollername
-            elif int(gewicht) <= 65 and counter65 <= 8:
-                datenspeicher.gewichtsklassen[65][vollername] = {}
-                datenspeicher.gewichtsklassen[65][vollername]["Gewicht"] = gewicht
-            elif int(gewicht) <= 70 and counter70 <= 8:
-                datenspeicher.gewichtsklassen[70][vollername] = {}
-                datenspeicher.gewichtsklassen[70][vollername]["Gewicht"] = gewicht
-            elif int(gewicht) <= 79 and counter79 <= 8:
-                datenspeicher.gewichtsklassen[79][vollername] = {}
-                datenspeicher.gewichtsklassen[79][vollername]["Gewicht"] = gewicht
-            elif int(gewicht) <= 96 and counter96 <= 8:
-                datenspeicher.gewichtsklassen[96][vollername] = {}
-                datenspeicher.gewichtsklassen[96][vollername]["Gewicht"] = gewicht
-            elif int(gewicht) <= 130 and counter130 <= 8:
-                datenspeicher.gewichtsklassen[130][vollername] = {}
-                datenspeicher.gewichtsklassen[130][vollername]["Gewicht"] = gewicht
+            # zufällige Loszuteilung der ringer, https://stackoverflow.com/questions/3996904/generate-random-integers-between-0-and-9
+            while aktiveslos in loszuteilung:
+                aktiveslos = randrange(1, 100)
+            losnummer = aktiveslos
+            loszuteilung.append(losnummer)
+            # 1. Eintrag Ringer ins Json übertragen
+            ringerdatenbank_dict.append({"ringerID": ringerID, "vorname": vorname, "nachname": nachname, "land": land, "turniere": [
+                {"weltmeisterschaft_freistil": {"gewicht": int(gewicht), "losnummer": losnummer}}]})
+            with open('ringerdatenbank.json', 'w') as f:
+                json.dump(ringerdatenbank_dict, f, indent=4, separators=(',', ':'), sort_keys=True)
+            print(loszuteilung)
+    return render_template("index.html")
 
-            # Überprüfung, ob in jedem Gewicht schon 8 Ringer sind
-            for i in datenspeicher.gewichtsklassen[57]:
-                counter57 = counter57 + 1
-            for i in datenspeicher.gewichtsklassen[65]:
-                counter65 = counter65 + 1
-            for i in datenspeicher.gewichtsklassen[70]:
-                counter70 = counter70 + 1
-            for i in datenspeicher.gewichtsklassen[79]:
-                counter79 = counter79 + 1
-            for i in datenspeicher.gewichtsklassen[96]:
-                counter96 = counter96 + 1
-            for i in datenspeicher.gewichtsklassen[130]:
-                counter130 = counter130 + 1
-
-            # Überprüfung, ob die Limite von 48 Ringern überschritten worden ist (6 Gewichtsklassen à 8 Teilnehmer)
-            while gesamtcounter <= 48:
-                return render_template("index.html")
-            return datenspeicher.gewichtsklassen
-        elif request.form.get("gewichteanzeigen") == "Gewichte Anzeigen":
-            return datenspeicher.gewichtsklassen
-    else:
-        return render_template("index.html")
 
 
 @app.route("/gewichterliste")
 def gewichterliste():
-    with open('gewichtsklassen.json', 'w') as f:
-        datenspeicher.json.dump(datenspeicher.gewichtsklassen, f, indent=4, separators=(',', ':'), sort_keys=True)
-    # idee, so zu machen von https://stackoverflow.com/questions/5750664/python-iterating-through-a-dictionary-gives-me-int-object-not-iterable
-    return render_template("gewichterliste.html", gewichtsklassen57=datenspeicher.gewichtsklassen[57],
-                           gewichtsklassen65=datenspeicher.gewichtsklassen[65], gewichtsklassen70=datenspeicher.gewichtsklassen[70],
-                           gewichtsklassen79=datenspeicher.gewichtsklassen[79], gewichtsklassen96=datenspeicher.gewichtsklassen[96],
-                           gewichtsklassen130=datenspeicher.gewichtsklassen[130])
+    r = open("ringerdatenbank.json")
+    ringerdatenbank_dict = json.load(r)
+    t = open("turnierdatenbank.json")
+    turnierdatenbank_dict = json.load(t)
+    ringerindex = 0
+    gewichtseinteilung = 0
+    anz_gewichtsklassen = len(turnierdatenbank_dict[0]["2021"][0]["weltmeisterschaft_freistil"]["gewichtsklassen"])
+    gewichtsklassenindex = 0
+
+#die ringer mit den gewichten in Gewichtsklassen einteilen
+    for ringer in ringerdatenbank_dict:
+        gewichtseinteilung = ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewicht"]
+        ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewichtsklasse"] = ""
+        while ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewichtsklasse"] == "":
+            for gewicht in turnierdatenbank_dict[0]["2021"][0]["weltmeisterschaft_freistil"]["gewichtsklassen"]:
+                if gewichtseinteilung == turnierdatenbank_dict[0]["2021"][0]["weltmeisterschaft_freistil"]["gewichtsklassen"][gewichtsklassenindex]["gewicht"]:
+                    ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewichtsklasse"] = gewichtseinteilung
+                else:
+                    gewichtsklassenindex = gewichtsklassenindex + 1
+            gewichtsklassenindex = 0
+            gewichtseinteilung = gewichtseinteilung + 1
+        ringerindex = ringerindex + 1
+    with open('ringerdatenbank.json', 'w') as r:
+        json.dump(ringerdatenbank_dict, r, indent=4, separators=(',', ':'), sort_keys=True)
+    with open('turnierdatenbank.json', 'w') as t:
+        json.dump(turnierdatenbank_dict, t, indent=4, separators=(',', ':'), sort_keys=True)
+# alle ringer für html in tuple
+    ringerindex = 0
+    output_ringer_fix = ()
+    for ringer in ringerdatenbank_dict:
+        output_ringer_temp = ((ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewichtsklasse"], ringerdatenbank_dict[ringerindex]["vorname"], ringerdatenbank_dict[ringerindex]["nachname"], ringerdatenbank_dict[ringerindex]["land"]),)
+        output_ringer_fix = output_ringer_fix + output_ringer_temp
+        ringerindex = ringerindex + 1
+
+    return render_template("gewichterliste.html", ringerliste=output_ringer_fix)
 
 
 @app.route("/matte1", methods=["GET", "POST"])
 def matte1():
-    index = 0
-    indexodd = 1
-    indexeven = 0
-    activewrestler1 = ""
-    activewrestler2 = ""
-    if request.method == "POST":
-        if request.form.get("kampfbeenden") == "Kampf beenden":
-            indexodd = indexodd + 2
-            indexeven = indexeven + 2
-            print(indexeven, indexodd)
-            for ringer in datenspeicher.gewichtsklassen[57]:
-                if indexeven == datenspeicher.gewichtsklassen[57][ringer]["Startnummer"]:
-                    activewrestler1 = datenspeicher.gewichtsklassen[57][ringer]["Name"]
-                elif indexodd == datenspeicher.gewichtsklassen[57][ringer]["Startnummer"]:
-                    activewrestler2 = datenspeicher.gewichtsklassen[57][ringer]["Name"]
-
-            print(activewrestler1, activewrestler2)
-            return render_template("matte1.html", activewrestler1=activewrestler1, activewrestler2=activewrestler2)
-    for gewicht in datenspeicher.gewichtsklassen:
-        for ringer in datenspeicher.gewichtsklassen[gewicht]:
-            datenspeicher.gewichtsklassen[gewicht][ringer]["Startnummer"] = index
-            index = index + 1
-        index = 1
-    print(datenspeicher.gewichtsklassen[57])
-    for ringer in datenspeicher.gewichtsklassen[57]:
-        if indexeven == datenspeicher.gewichtsklassen[57][ringer]["Startnummer"]:
-            activewrestler1 = datenspeicher.gewichtsklassen[57][ringer]["Name"]
-        elif indexodd == datenspeicher.gewichtsklassen[57][ringer]["Startnummer"]:
-            activewrestler2 = datenspeicher.gewichtsklassen[57][ringer]["Name"]
-
-    print(activewrestler1, activewrestler2)
-    return render_template("matte1.html", activewrestler1=activewrestler1, activewrestler2=activewrestler2)
+    return render_template("matte1.html")
 
 
 @app.route("/matte2")
