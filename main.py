eb9728b653350c7641a14c58ffecfd5a5aa16e73
jadonic <21343764+jadonic@users.noreply.github.com>
2021-12-21 16:07:45 +0100

    Optimierung Gewichtszuteilung, Zuteilung ringerID und Darstellung Gewichterliste

--- main.py
+++ main.py
@@ -17,11 +17,13 @@ def hello_post():
 
     loszuteilung = [0]
     aktiveslos = 0
-    ringerID = 100000
 
     if request.method == "POST":
         if request.form.get("eintragen") == "Eintragen":
-            ringerID = ringerID + 1
+            if ringerdatenbank_dict != []:
+                ringerID = ringerdatenbank_dict[-1]["ringerID"] + 1
+            elif ringerdatenbank_dict == []:
+                ringerID = 100000
             # nimmt daten von erstem Formular entgegen
             vorname = request.form["vorname"]
             nachname = request.form["nachname"]
@@ -33,13 +35,13 @@ def hello_post():
             losnummer = aktiveslos
             loszuteilung.append(losnummer)
             # 1. Eintrag Ringer ins Json übertragen
-            ringerdatenbank_dict.append({"ringerID": ringerID, "vorname": vorname, "nachname": nachname, "land": land, "turniere": [
-                {"weltmeisterschaft_freistil": {"gewicht": int(gewicht), "losnummer": losnummer}}]})
+            ringerdatenbank_dict.append(
+                {"ringerID": ringerID, "vorname": vorname, "nachname": nachname, "land": land, "turniere": [
+                    {"weltmeisterschaft_freistil": {"gewicht": int(gewicht), "losnummer": losnummer}}]})
             with open('ringerdatenbank.json', 'w') as f:
                 json.dump(ringerdatenbank_dict, f, indent=4, separators=(',', ':'), sort_keys=True)
             print(loszuteilung)
     return render_template("index.html")
-
 
 
 @app.route("/gewichterliste")
@@ -53,14 +55,16 @@ def gewichterliste():
     anz_gewichtsklassen = len(turnierdatenbank_dict[0]["2021"][0]["weltmeisterschaft_freistil"]["gewichtsklassen"])
     gewichtsklassenindex = 0
 
-#die ringer mit den gewichten in Gewichtsklassen einteilen
-    for ringer in ringerdatenbank_dict:
-        gewichtseinteilung = ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewicht"]
-        ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewichtsklasse"] = ""
-        while ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewichtsklasse"] == "":
-            for gewicht in turnierdatenbank_dict[0]["2021"][0]["weltmeisterschaft_freistil"]["gewichtsklassen"]:
-                if gewichtseinteilung == turnierdatenbank_dict[0]["2021"][0]["weltmeisterschaft_freistil"]["gewichtsklassen"][gewichtsklassenindex]["gewicht"]:
+    # die ringer mit den gewichten in Gewichtsklassen einteilen
+    for ringer in ringerdatenbank_dict: #für jeden Ringer
+        gewichtseinteilung = ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewicht"] #Variable "gewichtseintelung" gesetzt, damit der wert im json unverändert bleibt
+        ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewichtsklasse"] = "" #leeren wwert der Gewichtsklasse bei jedem Ringer hinterlegt, damit der Wert überprüft werden kann
+        while ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewichtsklasse"] == "": #solange dieser Wert (siehe Zeile oben) gleich "" ist muss wiederholt werden
+            for gewicht in turnierdatenbank_dict[0]["2021"][0]["weltmeisterschaft_freistil"]["gewichtsklassen"]: #für jedes Gewicht bei den Erfassten Gewichtern in der Turnierdatenbank
+                if gewichtseinteilung == turnierdatenbank_dict[0]["2021"][0]["weltmeisterschaft_freistil"]["gewichtsklassen"][gewichtsklassenindex]["gewicht"]: #stimmt das Prüfgewicht (gewichtseinteilung) mit der Gewichtsklasse in der TurnierDB überein, dann kann dieses eingetragen werden
                     ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewichtsklasse"] = gewichtseinteilung
+                    if ringerdatenbank_dict[ringerindex]["ringerID"] not in turnierdatenbank_dict[0]["2021"][0]["weltmeisterschaft_freistil"]["gewichtsklassen"][gewichtsklassenindex]["teilnehmer"]: #damit die ringerIDs nur einmal in der Turnierdatenbank erfasst werden
+                        turnierdatenbank_dict[0]["2021"][0]["weltmeisterschaft_freistil"]["gewichtsklassen"][gewichtsklassenindex]["teilnehmer"].append(ringerdatenbank_dict[ringerindex]["ringerID"]) #ordnet ringerID dem entsprechendem Gewicht zu.
                 else:
                     gewichtsklassenindex = gewichtsklassenindex + 1
             gewichtsklassenindex = 0
@@ -70,13 +74,18 @@ def gewichterliste():
         json.dump(ringerdatenbank_dict, r, indent=4, separators=(',', ':'), sort_keys=True)
     with open('turnierdatenbank.json', 'w') as t:
         json.dump(turnierdatenbank_dict, t, indent=4, separators=(',', ':'), sort_keys=True)
-# alle ringer für html in tuple
+    # alle ringer für html in tuple
     ringerindex = 0
     output_ringer_fix = ()
     for ringer in ringerdatenbank_dict:
-        output_ringer_temp = ((ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"]["gewichtsklasse"], ringerdatenbank_dict[ringerindex]["vorname"], ringerdatenbank_dict[ringerindex]["nachname"], ringerdatenbank_dict[ringerindex]["land"]),)
+        output_ringer_temp = ((ringerdatenbank_dict[ringerindex]["turniere"][0]["weltmeisterschaft_freistil"][
+                                   "gewichtsklasse"], ringerdatenbank_dict[ringerindex]["vorname"],
+                               ringerdatenbank_dict[ringerindex]["nachname"],
+                               ringerdatenbank_dict[ringerindex]["land"]),)
         output_ringer_fix = output_ringer_fix + output_ringer_temp
         ringerindex = ringerindex + 1
+    #habe ich von https://www.gkindex.com/python-tutorial/python-nested-tuples.jsp
+    output_ringer_fix = sorted(output_ringer_fix, key=lambda x: x[0])
 
     return render_template("gewichterliste.html", ringerliste=output_ringer_fix)
 
